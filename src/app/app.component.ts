2f904058773c86548100c81eee4ea5aee9a73e86
pcpradeep <pc.pradeep6@gmail.com>
2019-06-05 20:24:29 +0530

    1 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
+  courses:any;
+  constructor(private http:HttpClient){}
+  ngOnInit(){
+    this.http.get('https://training-search-node-app.appspot.com/getAllCourses').subscribe((data)=>{
+          this.courses=data;
+    });
+  }
+
+  getCourses(loc){
+    this.http.get('https://training-search-node-app.appspot.com/allcoursesbyloc/'+loc).subscribe((data)=>{
+        this.courses=data;
+    });
+  }
 }
